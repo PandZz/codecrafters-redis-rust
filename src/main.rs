766fb9c28608123bb9359e15cf1fd9f3c7de09fc
Zhao Junlang <2739468442@qq.com>
2024-04-11 23:18:05 +0800

    pass 2.11 stage

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,18 @@ use std::{
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    sync::Mutex,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Mutex,
+    },
 };
 
 type ShardedDb = Arc<Vec<Mutex<HashMap<String, (RESP, u128)>>>>;
+type CmdSender = Sender<RESP>;
+type CmdReceiver = Receiver<RESP>;
+type ReplicaSender = Sender<Vec<u8>>;
+// type ReplicaRecevier = Receiver<Vec<u8>>;
+type ShardedTxList = Arc<Mutex<Vec<ReplicaSender>>>;
 
 static EMPTY_RDB_HEX: &'static str = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
 
@@ -56,12 +64,35 @@ fn hex2bytes(src: &str) -> Vec<u8> {
         .collect()
 }
 
-async fn handle_client(mut stream: TcpStream, db: ShardedDb, config: Arc<Mutex<Config>>) {
+async fn handle_replica(mut stream: TcpStream, mut rx: Receiver<Vec<u8>>) {
+    loop {
+        if let Some(val) = rx.recv().await {
+            stream.write_all(&val).await.unwrap()
+        }
+    }
+}
+
+async fn handle_trans_write_cmd(mut cmd_rx: CmdReceiver, tx_list: ShardedTxList) {
+    loop {
+        if let Some(cmd) = cmd_rx.recv().await {
+            let tx_list = tx_list.lock().await;
+            let cmd: Vec<_> = cmd.to_string().bytes().collect();
+            for tx in tx_list.iter() {
+                tx.send(cmd.clone()).await.unwrap();
+            }
+        }
+    }
+}
+
+async fn handle_client(
+    mut stream: TcpStream,
+    db: ShardedDb,
+    config: Arc<Mutex<Config>>,
+    tx_list: ShardedTxList,
+    write_cmd_tx: CmdSender,
+) {
     let resp_null_bytes = RESP::Null.to_string();
     let resp_null_bytes = resp_null_bytes.as_bytes();
-    let empty_rdb_bytes = hex2bytes(EMPTY_RDB_HEX);
-    let front = format!("${}\r\n", empty_rdb_bytes.len());
-    let empty_rdb_bytes = vec![front.into_bytes(), empty_rdb_bytes].concat();
     let mut buf = [0; 512];
     loop {
         let count = stream.read(&mut buf).await.unwrap();
@@ -70,6 +101,7 @@ async fn handle_client(mut stream: TcpStream, db: ShardedDb, config: Arc<Mutex<C
         }
         if let Some((_, resp)) = RESP::read_next_resp(&buf) {
             if let Some(cmd) = Cmd::from(&resp) {
+                let mut is_write_cmd = false;
                 let res;
                 let response = match cmd {
                     Cmd::Ping => "+PONG\r\n".as_bytes(),
@@ -89,6 +121,7 @@ async fn handle_client(mut stream: TcpStream, db: ShardedDb, config: Arc<Mutex<C
                         }
                         db.insert(key, (RESP::new_bulk(value), expire_time));
                         res = RESP::new_simple("OK".to_string()).to_string();
+                        is_write_cmd = true;
                         res.as_bytes()
                     }
                     Cmd::Get(key) => {
@@ -137,20 +170,29 @@ async fn handle_client(mut stream: TcpStream, db: ShardedDb, config: Arc<Mutex<C
                                 )
                                 .await
                                 .unwrap();
-                            &empty_rdb_bytes
-                        } else {
-                            resp_null_bytes
+                            let empty_rdb_bytes = hex2bytes(EMPTY_RDB_HEX);
+                            let front = format!("${}\r\n", empty_rdb_bytes.len());
+                            let empty_rdb_bytes =
+                                vec![front.into_bytes(), empty_rdb_bytes].concat();
+                            stream.write_all(&empty_rdb_bytes).await.unwrap();
                         }
+                        let (tx, rx) = mpsc::channel(32);
+                        tx_list.lock().await.push(tx);
+                        tokio::spawn(handle_replica(stream, rx));
+                        break;
                     }
                     _ => resp_null_bytes,
                 };
                 stream.write_all(response).await.unwrap();
+                if is_write_cmd {
+                    write_cmd_tx.send(resp).await.unwrap();
+                }
             }
         }
     }
 }
 
-async fn handshake(config: Arc<Mutex<Config>>) -> Result<()> {
+async fn handshake(config: Arc<Mutex<Config>>) -> Result<TcpStream> {
     let mut config = config.lock().await;
     // handshake
     let mut stream =
@@ -206,7 +248,7 @@ async fn handshake(config: Arc<Mutex<Config>>) -> Result<()> {
         config.master_replid = repl_id;
         config.master_repl_offset = offset;
         println!("handshake: REPLCONF finished");
-        Ok(())
+        Ok(stream)
     } else {
         Err(anyhow!(
             "failed in 2.6 receive \"+FULLRESYNC <REPL_ID> 0\r\n\" from master"
@@ -214,10 +256,36 @@ async fn handshake(config: Arc<Mutex<Config>>) -> Result<()> {
     }
 }
 
-async fn handle_master(config: Arc<Mutex<Config>>) -> Result<()> {
+async fn handle_master(config: Arc<Mutex<Config>>, db: ShardedDb) -> Result<()> {
     if config.lock().await.role.as_str() == "slave" {
-        handshake(config.clone()).await?;
-        println!("slave: handshake finished");
+        let mut stream = handshake(config.clone()).await?;
+        println!("slave: handshake has finished, listening from master begins");
+        let mut buf = [0; 512];
+        loop {
+            let count = stream.read(&mut buf).await.unwrap();
+            if count == 0 {
+                break;
+            }
+            if let Some((_, resp)) = RESP::read_next_resp(&buf) {
+                if let Some(cmd) = Cmd::from(&resp) {
+                    match cmd {
+                        Cmd::Set(key, value, mut expire_time) => {
+                            let shard = hash(&key) % db.len();
+                            let now_millis = SystemTime::now()
+                                .duration_since(UNIX_EPOCH)
+                                .unwrap()
+                                .as_millis();
+                            let mut db = db[shard].lock().await;
+                            if expire_time != u128::MAX {
+                                expire_time += now_millis
+                            }
+                            db.insert(key, (RESP::new_bulk(value), expire_time));
+                        }
+                        _ => (),
+                    };
+                }
+            }
+        }
     } else {
         println!("master: no need for handshaking");
     }
@@ -228,8 +296,10 @@ async fn handle_master(config: Arc<Mutex<Config>>) -> Result<()> {
 async fn main() {
     let args = env::args();
     let config = Arc::new(Mutex::new(Config::from_args(args)));
+    let db = new_sharded_db(32);
 
-    tokio::spawn(handle_master(config.clone()));
+    // 只有当前服务器为slave时, 这里能连接到1个master服务器, 在这里接收到的"write"命令只需静默执行
+    tokio::spawn(handle_master(config.clone(), db.clone()));
 
     let listener = {
         let read_config = config.lock().await;
@@ -238,13 +308,19 @@ async fn main() {
             .unwrap()
     };
 
-    let db = new_sharded_db(32);
+    let (cmd_tx, cmd_rx) = mpsc::channel(512);
+    let replica_tx_list: ShardedTxList = Arc::new(Mutex::new(Vec::new()));
 
+    tokio::spawn(handle_trans_write_cmd(cmd_rx, replica_tx_list.clone()));
     loop {
+        // 若当前服务器为master, 则: 在n个stream中有m个是客户端, n - m个是slave服务器, 需要将客户端发来的"write"命令转发到slave服务器
+        // 若当前服务器为slave, 则: 在此处的stream全都是客户端, 无需特殊处理
         let (stream, _) = listener.accept().await.unwrap();
         println!("accepted new connection");
         let db = db.clone();
         let config = config.clone();
-        tokio::spawn(handle_client(stream, db, config));
+        let replica_tx_list = replica_tx_list.clone();
+        let cmd_tx = cmd_tx.clone();
+        tokio::spawn(handle_client(stream, db, config, replica_tx_list, cmd_tx));
     }
 }
